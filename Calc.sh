17a386a9342248aaee0ad3dc93ff86ab40a97750
Nilsu11 <106374285+Nilsu11@users.noreply.github.com>
2022-10-22 17:15:19 +0200

    Update Calc.sh

--- Calc.sh
+++ Calc.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/LineageOS/android_packages_apps_ExactCalculator.git
 git clone git:github.com/Nilsu11/ExactCalcGradle.git android_packages_apps_ExactCalculator/gradle
 sed -i '/package/a import org.lineageos.calculator2.R;' android_packages_apps_ExactCalculator/src/com/android/calculator2/**.java
+git clone https://android.googlesource.com/platform/external/crcalc
