66f452ae965420da2cf3d04c68e82b2de882c434
Nilsu11 <106374285+Nilsu11@users.noreply.github.com>
2022-10-25 15:23:17 +0200

    use com.android

--- Calc.sh
+++ Calc.sh
@@ -1,2 +1,2 @@
-sed -i '/package/a import org.lineageos.calculator2.R;' android_packages_apps_ExactCalculator/src/com/android/calculator2/**.java
+sed -i '/package/a import com.android.calculator2.R;' android_packages_apps_ExactCalculator/src/com/android/calculator2/**.java
 git clone https://android.googlesource.com/platform/external/crcalc
