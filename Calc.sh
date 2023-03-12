7e91fde9a59a9848e9b42e68c8fee4c1ad641f5e
Nilsu11 <106374285+Nilsu11@users.noreply.github.com>
2023-03-12 13:21:30 +0100

    use net.crdroid to make it installable
    add some command improvements

--- Calc.sh
+++ Calc.sh
@@ -1,2 +1,4 @@
-sed -i '/package/a import com.android.calculator2.R;' android_packages_apps_ExactCalculator/src/com/android/calculator2/**.java
-git clone https://android.googlesource.com/platform/external/crcalc
+git clone https://github.com/crdroidandroid/android_packages_apps_ExactCalculator.git ~/modLOS/ExactCalculator
+git clone https://github.com/Nilsu11/ExactCalcGradle.git ~/modLOS/ExactCalculator/Gradle
+sed -i '/package/a import net.crdroid.calculator2.R;' android_packages_apps_ExactCalculator/src/com/android/calculator2/**.java
+git clone https://android.googlesource.com/platform/external/crcalc ~/modLOS/ExactCalculator/crcalc
