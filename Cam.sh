2afe7f921676d19335cd894f3ba993ec99f340ed
Nilsu11 <106374285+Nilsu11@users.noreply.github.com>
2024-03-03 11:58:00 +0100

    Update Cam.sh

--- Cam.sh
+++ Cam.sh
@@ -1,2 +1,3 @@
 git clone https://github.com/LineageOS/android_packages_apps_Camera2.git ~/modLOS/Camera2
 git clone https://github.com/Nilsu11/apps_Gradle.git -b Cam2 ~/modLOS/Camera2/Gradle
+git clone https://github.com/LineageOS/android_framework_ex.git ~/modLOS/Camera2/Gradle/ex
